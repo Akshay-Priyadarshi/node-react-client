2d3cad526b53c92e27682262f9f4d8d3bcb23a36
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-06-10 12:35:43 +0530

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider as ReduxProvider } from "react-redux";
+import App from "./App";
+import { customTheme } from "./theme";
+import customStore from "./store";
+import axios from "axios";
+import "./index.css";
+
+const NODE_ENV = process.env.NODE_ENV;
+const BASE_URL =
+    NODE_ENV && NODE_ENV === "development"
+        ? process.env.REACT_APP_DEV_API_URL
+        : process.env.REACT_APP_PROD_API_URL;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ReduxProvider store={customStore}>
+            <BrowserRouter>
+                <ChakraProvider resetCSS theme={customTheme}>
+                    <App />
+                </ChakraProvider>
+            </BrowserRouter>
+        </ReduxProvider>
+    </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+axios.defaults.baseURL = BASE_URL + "/api";
+serviceWorkerRegistration.register();
 reportWebVitals();
